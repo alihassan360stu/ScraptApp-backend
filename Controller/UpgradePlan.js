b68e6371b48e92797fcde15b4860143cd064ce80
ali hassan <ali.hassan.stu@gmail.com>
2023-04-14 04:34:14 +0500

    email changed now

--- Controller/UpgradePlan.js
+++ Controller/UpgradePlan.js
@@ -11,8 +11,8 @@ async function sendSubscribedMail(data, url) {
         port: 587,
         secure: false,
         auth: {
-            user: "Scrapsterremoval@gmail.com", // generated ethereal user
-            pass: "yixthwkgdiixnknq", // generated ethereal password
+            user: "scrapter14@gmail.com", // generated ethereal user
+            pass: "mcghgwyzwvhmfpui", // generated ethereal password
         },
     });
 
@@ -33,8 +33,8 @@ async function sendMailToMerchent(data, url) {
         port: 587,
         secure: false,
         auth: {
-            user: "Scrapsterremoval@gmail.com", // generated ethereal user
-            pass: "yixthwkgdiixnknq", // generated ethereal password
+            user: "scrapter14@gmail.com", // generated ethereal user
+            pass: "mcghgwyzwvhmfpui", // generated ethereal password
         },
     });
 
